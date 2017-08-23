e55a3436d923c3fc7f2318bc01d3cc4b000b41e2
FiroTest <firoja.begam88@gmail.com>
2017-08-23 18:33:57 +0530

    added method

--- src/main/java/com/api/Chat/controller/rest/HelloWorldController.java
+++ src/main/java/com/api/Chat/controller/rest/HelloWorldController.java
@@ -35,7 +35,7 @@ public class HelloWorldController {
 		String action = getAction(jsonData);
 		
 		if(action.equalsIgnoreCase("getUser")||action.equalsIgnoreCase("getRegistration")||action.equalsIgnoreCase("getFacility")||action.equalsIgnoreCase("getTraining")
-				|| action.equalsIgnoreCase("getStudyWorkspace")||action.equalsIgnoreCase("getSurvey")){
+				|| action.equalsIgnoreCase("getStudyWorkspace")||action.equalsIgnoreCase("getSurvey")||action.equalsIgnoreCase("getReports")||action.equalsIgnoreCase("getDocuments")){
 		CloseableHttpClient httpclient = HttpClients.custom()
 		         .build();
 		String responseBody = null;
